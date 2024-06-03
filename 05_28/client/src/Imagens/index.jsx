05526115ac222810f48b481c516f5565bca6d043
Ramon de Holanda <ramonhnascimento@gmail.com>
2024-06-03 15:51:58 -0300

    Inserindo imagens

---
+++ 05_28/client/src/Imagens/index.jsx
@@ -0,0 +1,10 @@
+import abobora from "./arquivos/abobora.jpg";
+import abacaxi from "./arquivos/abacaxi.webp"
+
+const imagensArray = {
+    abobora,
+    abacaxi
+}
+
+
+export default imagensArray;
--- 05_28/client/src/views/Home/index.jsx
+++ 05_28/client/src/views/Home/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import "./estilo.css"
+import imagensArray from '../../Imagens'
+import senai from "./senai.png"
 function Home() {
   return (
-    <div>
+    <div className='vermelho'>
+      {/*asdasdasd */}
+        <img src={senai} />
+        <img src={imagensArray.abobora} alt="" />
+        <img src={imagensArray.abacaxi} alt="" />
         <h1>Bem Vindo a página inicial</h1>
     </div>
   )
